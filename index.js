52170a2c356ef48ffd5193569d25751b4c5ceb3a
imnotoriginal <imnotoriginal@users.noreply.github.com>
2020-07-13 15:23:14 +0300

    add auth

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const jwt = require("jsonwebtoken");
 const app = express();
+const KEY = "secret_token";
 
 app.get("/api", (req, res) => {
   res.json({
@@ -8,9 +9,23 @@ app.get("/api", (req, res) => {
   });
 });
 
-app.post("/api/protected", (req, res) => {
-  res.json({
-    message: "It is protected route",
+const verify = (req, res, next) => {
+  const bearerHeader = req.headers["authorization"];
+  if (!bearerHeader) res.sendStatus(403);
+  req.token = bearerHeader.split(" ")[1];
+  next();
+};
+
+app.post("/api/protected", verify, (req, res) => {
+  jwt.verify(req.token, KEY, (err, data) => {
+    if (err) {
+      console.log(err);
+      res.sendStatus(403);
+    }
+    res.json({
+      message: "It is protected route",
+      data,
+    });
   });
 });
 
@@ -21,8 +36,11 @@ app.post("/api/login", (req, res) => {
     _id: "fdgjo34j5435ko",
   };
 
-  jwt.sign({ mockUser }, "secretKey", (err, token) => {
-    if (err) console.log(err);
+  jwt.sign({ mockUser }, KEY, (err, token) => {
+    if (err) {
+      console.log(err);
+      res.sendStatus(500);
+    }
     res.json({
       message: "Successful",
       token,
