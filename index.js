b9e2f91a00fe305e4354f1155a0cc28626d1f99e
imnotoriginal <imnotoriginal@users.noreply.github.com>
2020-07-13 15:36:11 +0300

    fix app port choosing

--- index.js
+++ index.js
@@ -52,4 +52,6 @@ app.post("/api/login", (req, res) => {
   });
 });
 
-app.listen(3000, () => console.log("[Server]: Started http://localhost:3000"));
+app.listen(process.env.PORT || 3000, () =>
+  console.log("[Server]: Started http://localhost:3000")
+);
